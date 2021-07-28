a7d117e49e32633894d7ead61bc2fd7617e0508f
Matheus Alano Joenck <matheusalanojoenck@gmail.com>
2021-07-28 18:08:25 -0300

    Classe que faz a manipulação da tabela estudande_pos
    Criado métodos para realizar queries básicas (select, insert, delete) na tabela estudante_pos

--- src/apresentacao/Main.java
+++ src/apresentacao/Main.java
@@ -1,17 +1,17 @@
 package apresentacao;
 
 import dados.Departamento;
+import dados.EstudantePos;
 import dados.Professor;
 import persistencia.DepartamentoDAO;
+import persistencia.EstudantePosDAO;
 import persistencia.ProfessorDAO;
 
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        ProfessorDAO professorDAO = ProfessorDAO.getInstance();
-        DepartamentoDAO departamentoDAO = DepartamentoDAO.getInstance();
-
+//        ProfessorDAO professorDAO = ProfessorDAO.getInstance();
 //        professorDAO.insert(2, "Matheus", 24, "F-101", "computacao");
 //
 //        List p = professorDAO.selectAll();
@@ -21,14 +21,25 @@ public class Main {
 //        professorDAO.delete(2);
 //
 //        System.out.println(p.toString());
-
+//
+//        DepartamentoDAO departamentoDAO = DepartamentoDAO.getInstance();
 //        Departamento d = departamentoDAO.select(1);
 //        System.out.println(d.toString());
-
+//
 //        departamentoDAO.insert(4, "dep4", "escrt4", 1);
-        departamentoDAO.delete(4);
-        List d = departamentoDAO.selectAll();
-        System.out.println(d.toString());
+//        departamentoDAO.delete(4);
+//        List d = departamentoDAO.selectAll();
+//        System.out.println(d.toString());
+
+        EstudantePosDAO estudantePosDAO = EstudantePosDAO.getInstance();
+//        EstudantePos estudantePos = estudantePosDAO.select(1);
+//        System.out.println(estudantePos.toString());
+
+//        estudantePosDAO.insert(4, "ALuno4", 18, "Tipo_curso_1", 1, 1);
+
+//        estudantePosDAO.delete(4);
+        List e = estudantePosDAO.selectAll();
+        System.out.println(e.toString());
 
     }
 }
---
+++ src/persistencia/EstudantePosDAO.java
@@ -0,0 +1,109 @@
+package persistencia;
+
+import dados.Departamento;
+import dados.EstudantePos;
+import dados.Professor;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EstudantePosDAO {
+    private static EstudantePosDAO instance = null;
+    private PreparedStatement selectEstudantePos;
+    private PreparedStatement selectAllEstudantePos;
+    private PreparedStatement insertEstudantePos;
+    private PreparedStatement deleteEstudantePos;
+
+    public static EstudantePosDAO getInstance() {
+        if (instance == null) instance = new EstudantePosDAO();
+        return instance;
+    }
+
+    private EstudantePosDAO() {
+        Connection connection = DBConnection.getConnection();
+        try {
+            selectEstudantePos = connection.prepareStatement(
+                    "SELECT * " +
+                            "FROM estudante_pos " +
+                            "WHERE mat_est = ?");
+
+            selectAllEstudantePos = connection.prepareStatement(
+                    "SELECT * " +
+                            "FROM estudante_pos");
+
+            insertEstudantePos = connection.prepareStatement(
+                    "INSERT INTO estudante_pos (mat_est, nome, idade, tipo_curso, num_dep, mat_est_aconselhador)" +
+                            "VALUES (?, ?, ?, ?, ?, ?)");
+
+            deleteEstudantePos = connection.prepareStatement(
+                    "DELETE FROM estudante_pos " +
+                            "WHERE mat_est = ?");
+
+        } catch (SQLException | NullPointerException e) {
+            e.printStackTrace();
+            //System.out.println(e.toString());
+        }
+    }
+
+    public EstudantePos select(int mat_est) {
+        EstudantePos est = null;
+        ResultSet rs;
+        try {
+            selectEstudantePos.setInt(1, mat_est);
+            rs = selectEstudantePos.executeQuery();
+            if (rs.next()) {
+                est = new EstudantePos();
+                est.setMat_est(rs.getInt("mat_est"));
+                est.setNome(rs.getString("nome"));
+                est.setIdade(rs.getInt("idade"));
+                est.setTipo_curso(rs.getString("tipo_curso"));
+                est.setNum_dep(rs.getInt("num_dep"));
+                est.setMat_est_aconselhador(rs.getInt("mat_est_aconselhador"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return est;
+    }
+
+
+    public List<EstudantePos> selectAll(){
+        List<EstudantePos> estudantesPos = new ArrayList<>();
+        ResultSet rs;
+
+        try{
+            rs = selectAllEstudantePos.executeQuery();
+            while (rs.next()){
+                estudantesPos.add(select(rs.getInt(1)));
+            }
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return estudantesPos;
+    }
+
+    public void insert(int mat_est, String nome, int idade,  String tipo_curso, int num_dep,int mat_est_aconselhador){
+        try {
+            insertEstudantePos.setInt(1, mat_est);
+            insertEstudantePos.setString(2, nome);
+            insertEstudantePos.setInt(3, idade);
+            insertEstudantePos.setString(4, tipo_curso);
+            insertEstudantePos.setInt(5, num_dep);
+            insertEstudantePos.setInt(6, mat_est_aconselhador);
+
+            insertEstudantePos.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void delete(int mat_est){
+        try {
+            deleteEstudantePos.setInt(1, mat_est);
+
+            deleteEstudantePos.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
