52e51f6c7c6a9ed562e58cbe932ad23d51d5001d
Matheus Alano Joenck <matheusalanojoenck@gmail.com>
2021-07-28 16:12:16 -0300

    Classe para acesso de dados da tabela departamento
    Classe que implementa queries básicas (select, delete, insert) para tabela departamento

--- src/apresentacao/Main.java
+++ src/apresentacao/Main.java
@@ -1,6 +1,8 @@
 package apresentacao;
 
+import dados.Departamento;
 import dados.Professor;
+import persistencia.DepartamentoDAO;
 import persistencia.ProfessorDAO;
 
 import java.util.List;
@@ -8,16 +10,25 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         ProfessorDAO professorDAO = ProfessorDAO.getInstance();
+        DepartamentoDAO departamentoDAO = DepartamentoDAO.getInstance();
 
-        //professorDAO.insert(2, "Matheus", 24, "F-101", "computacao");
+//        professorDAO.insert(2, "Matheus", 24, "F-101", "computacao");
+//
+//        List p = professorDAO.selectAll();
+//
+//        System.out.println(p.toString());
+//
+//        professorDAO.delete(2);
+//
+//        System.out.println(p.toString());
 
-        List p = professorDAO.selectAll();
+//        Departamento d = departamentoDAO.select(1);
+//        System.out.println(d.toString());
 
-        //System.out.println(p.toString());
-
-        professorDAO.delete(2);
-
-        System.out.println(p.toString());
+//        departamentoDAO.insert(4, "dep4", "escrt4", 1);
+        departamentoDAO.delete(4);
+        List d = departamentoDAO.selectAll();
+        System.out.println(d.toString());
 
     }
 }
--- src/persistencia/DepartamentoDAO.java
+++ src/persistencia/DepartamentoDAO.java
@@ -1,4 +1,102 @@
 package persistencia;
 
+import dados.Departamento;
+import dados.Professor;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DepartamentoDAO {
+    private static DepartamentoDAO instance = null;
+    private PreparedStatement selectDepartamento;
+    private PreparedStatement selectAllDepartamento;
+    private PreparedStatement insertDepartamento;
+    private PreparedStatement deleteDepartamento;
+
+    public static DepartamentoDAO getInstance() {
+        if (instance == null) instance = new DepartamentoDAO();
+        return instance;
+    }
+
+    private DepartamentoDAO(){
+        Connection connection = DBConnection.getConnection();
+        try {
+            selectDepartamento = connection.prepareStatement(
+                    "SELECT * " +
+                        "FROM departamento " +
+                        "WHERE num_dep = ?");
+
+            selectAllDepartamento = connection.prepareStatement(
+                    "SELECT * " +
+                        "FROM departamento");
+
+            insertDepartamento = connection.prepareStatement(
+                    "INSERT INTO departamento (num_dep, nome, escritorio, mat_prof)" +
+                        "VALUES (?, ?, ?, ?)");
+
+            deleteDepartamento = connection.prepareStatement(
+                    "DELETE FROM departamento " +
+                        "WHERE num_dep = ?");
+
+        } catch (SQLException  | NullPointerException e){
+            e.printStackTrace();
+            //System.out.println(e.toString());
+        }
+    }
+
+    public Departamento select(int num_dep) {
+        Departamento d = null;
+        ResultSet rs;
+        try {
+            selectDepartamento.setInt(1, num_dep);
+            rs = selectDepartamento.executeQuery();
+            if (rs.next()){
+                d = new Departamento();
+                d.setNum_dep(rs.getInt("num_dep"));
+                d.setNome(rs.getString("nome"));
+                d.setEscritorio(rs.getString("escritorio"));
+                d.setMat_prof(rs.getInt("mat_prof"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return d;
+    }
+    public List<Departamento> selectAll(){
+        List<Departamento> departamentos = new ArrayList<>();
+        ResultSet rs;
+
+        try{
+            rs = selectAllDepartamento.executeQuery();
+            while (rs.next()){
+                departamentos.add(select(rs.getInt(1)));
+            }
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return departamentos;
+    }
+
+    public void insert(int num_dep, String nome, String escritorio, int mat_prof){
+        try {
+            insertDepartamento.setInt(1, num_dep);
+            insertDepartamento.setString(2, nome);
+            insertDepartamento.setString(3, escritorio);
+            insertDepartamento.setInt(4, mat_prof);
+
+            insertDepartamento.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void delete(int num_dep){
+        try {
+            deleteDepartamento.setInt(1, num_dep);
+
+            deleteDepartamento.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
